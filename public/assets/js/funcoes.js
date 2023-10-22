15526e56ab65cecca65fee358d7575f0e8d6405a
Yuji <kaikykohatsu@gmail.com>
2023-10-21 23:17:28 -0300

    Atualização nas dashboards

--- public/assets/js/funcoes.js
+++ public/assets/js/funcoes.js
@@ -1,46 +1,27 @@
+const horarioComercial = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 function criarGraficoFuncionario(){
   const horariosPico = {
     html: document.getElementById("horarios-pico"),
     titulo: "Máquinas com problema",
-    label: []
   }
 
   const data = {
-    datasets: 
-    [{
-      label: "Máquina 1",
-      data: [{
-        x: 0,
-        y: 0
-      },
-      {
-        x: 0,
-        y: 10
-      }]
-    },
-    {
-      label: "Máquina 2",
-      data: [{
-        x: 1,
-        y: 0
-      },
-      {
-        x: 1,
-        y: 10
-      }]
-    }
-    ]
+    labels: horarioComercial,
+    datasets: [{
+      label: "Quantidade de problemas",
+      data: [1, 3, 3,	4, 2, 3, 1, 2, 0, 3, 2, 2, 4, 3, 1, 2]
+    }]
   }
 
   new Chart(horariosPico.html, {
-    type: "scatter",
+    type: "line",
     data: data 
   });
 
 }
 
 function criarGraficoSuporte() {
-  const horarioComercial = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+  
 
   const disco = {
     html: document.getElementById("disco"),
